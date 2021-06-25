2e9dbd614b955020e747665d4e11967caf1e7098
Do Kim Phong <phong.dk176843@sis.hust.edu.vn>
2021-06-25 11:40:30 +0700

    remove nice scroll

--- js/main.js
+++ js/main.js
@@ -40,18 +40,6 @@ $(document).ready(function () {
   //parallax effect initialization
   $('.hero').parallax('50%', 0.3)
 
-  //Nice scroll initialization
-  $('html').niceScroll({
-    scrollspeed: 50,
-    autohidemode: false,
-    cursorwidth: 8,
-    cursorborderradius: 8,
-    cursorborder: '0',
-    background: 'rgba(48, 48, 48, .4)',
-    cursorcolor: '#1f1f1f',
-    zindex: 999,
-  })
-
   //Testimonials slider initialization
   $('#tslider').owlCarousel({
     items: 1,
